d258bfa2536078e6d3b2c292c986ca75f40abf84
LaraCassandra <55394473+LaraCassandra@users.noreply.github.com>
2021-03-03 23:39:23 +1100

    Add navigation
    Add navigation from landing page to categories page with name validation

--- QuizMe/app/src/main/java/com/example/quizme/MainActivity.kt
+++ QuizMe/app/src/main/java/com/example/quizme/MainActivity.kt
@@ -1,14 +1,43 @@
 package com.example.quizme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // MAKE APP FULLSCREEN
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // REFERENCE TO UI
+        val btn_start = findViewById<Button>(R.id.btn_start)
+        val et_name = findViewById<TextView>(R.id.et_name)
+
+        // START BUTTON FUNCTION
+        btn_start.setOnClickListener {
+
+            // NAME VALIDATION
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, CategoriesActivity::class.java).apply {
+                    putExtra(EXTRA_MESSAGE, et_name.text.toString())
+                }
+                startActivity(intent)
+                finish()
+            }
+
+        }
+
     }
 }
